07efb73ef6cfc1fd77b34958d2798d5db14b8f8b
Maya <foenx0315@gmail.com>
2023-12-04 19:34:15 +0530

    stuff

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+import { Poppins } from 'next/font/google'
+
+import { ThemeProvider } from '@/components/theme-provider'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+export const metadata: Metadata = {
+  title: 'SaaS App',
+  description: 'SaaS Template For',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className={poppins.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Container from '@/components/container'
+import Navbar from '@/components/navbar'
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Hero from '@/components/hero'
+import Features from '@/components/Features'
+
+export default function Home() {
+  return (
+    <Container>
+      <Navbar />
+      <Hero />
+      <Features />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+    </Container>
+  )
+}
